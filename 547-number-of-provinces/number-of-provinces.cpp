9497d1c77a14673d84fd2c8237134a93ba79b585
Krupal Butala <b24315@students.iitmandi.ac.in>
2026-05-23 17:36:14 +0530

    Time: 5 ms (16.65%) | Memory: 20.5 MB (31.52%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector <int>> &adj, vector<int> &visited) {
-        visited[node] = 1;
-        
-        for(auto it : adj[node]) {
-            if(!visited[it]) {
-                dfs(it, adj, visited);
+    void dfs(vector<vector <int>>& adj, vector <int>& vis, int node){
+        vis[node] = 1;
+        for (auto it : adj[node]){
+            if(!vis[it]){
+                dfs(adj,vis,it);
             }
         }
     }
-    
-    int findCircleNum(vector<vector<int>>& isconnected) {
-        int n = isconnected.size();
-        
-        // FIX 1: Initialize adjacency list with 'n' empty rows
-        vector<vector<int>> adj(n); 
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isconnected[i][j] == 1 && i != j) {
-                    // FIX 2: Only add the one direction. The loop will 
-                    // catch the reverse direction on its own.
-                    adj[i].push_back(j); 
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector <vector<int>> adj(n);
+
+        for (int i= 0;i<n;i++){
+            for(int j = 0; j<n;j++){
+                if(isConnected[i][j] == 1 && i!= j){
+                    adj[i].push_back(j);
                 }
             }
         }
-        
-        vector<int> visited(n, 0);
         int cnt = 0;
-        
-        for (int i = 0; i < n; i++) {
-            if(visited[i] == 0) {
+        vector  <int> vis(n,0);
+        for(int i = 0 ;i <n;i++){
+            if(!vis[i]){
                 cnt++;
-                dfs(i, adj, visited);
+                dfs(adj, vis, i);
             }
         }
-        
         return cnt;
     }
 };
