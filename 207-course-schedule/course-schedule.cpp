71e8c9de461b33737478b4ed6c6acede19a05f1e
Krupal Butala <b24315@students.iitmandi.ac.in>
2026-05-29 19:13:03 +0530

    Time: 7 ms (46.04%) | Memory: 19.5 MB (48.27%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,54 @@
+class Solution {
+private:
+    bool dfs(int node,
+             vector<int>& vis,
+             vector<int>& pathvis,
+             vector<vector<int>>& adj) {
+
+        vis[node] = 1;
+        pathvis[node] = 1;
+
+        for(auto it : adj[node]) {
+
+            if(!vis[it]) {
+
+                if(dfs(it, vis, pathvis, adj))
+                    return true;
+            }
+            else if(pathvis[it]) {
+
+                return true;
+            }
+        }
+
+        pathvis[node] = 0;   // Backtrack
+
+        return false;
+}
+public:
+    bool canFinish(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+
+        for(auto &e : edges) {
+
+            int u = e[0];
+            int v = e[1];
+
+            adj[u].push_back(v);
+        }
+
+        vector<int> vis(V, 0);
+        vector<int> pathvis(V, 0);
+
+        for(int i = 0; i < V; i++) {
+
+            if(!vis[i]) {
+
+                if(dfs(i, vis, pathvis, adj))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
