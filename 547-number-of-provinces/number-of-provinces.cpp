bbee1c0390d2b46e26c2d5c8000ee77b3280e683
Krupal Butala <b24315@students.iitmandi.ac.in>
2026-06-17 01:15:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (72.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,53 @@
-class Solution {
+class DSU {
 public:
-    void dfs(vector<vector <int>>& adj, vector <int>& vis, int node){
-        vis[node] = 1;
-        for (auto it : adj[node]){
-            if(!vis[it]){
-                dfs(adj,vis,it);
-            }
+    vector<int> parent, size;
+
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int findParent(int node) {
+        if (parent[node] == node) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if (pu == pv) return;
+
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
         }
     }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector <vector<int>> adj(n);
+        DSU dsu(n);
 
-        for (int i= 0;i<n;i++){
-            for(int j = 0; j<n;j++){
-                if(isConnected[i][j] == 1 && i!= j){
-                    adj[i].push_back(j);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j]) {
+                    dsu.unionBySize(i, j);
                 }
             }
         }
-        int cnt = 0;
-        vector  <int> vis(n,0);
-        for(int i = 0 ;i <n;i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(adj, vis, i);
-            }
+
+        int provinces = 0;
+        for (int i = 0; i < n; i++) {
+            if (dsu.findParent(i) == i) provinces++;
         }
-        return cnt;
+
+        return provinces;
     }
 };
