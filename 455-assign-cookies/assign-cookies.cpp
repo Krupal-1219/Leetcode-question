cfc5ff710216b2f48d1f361a7744c98c7d5d9045
Krupal Butala <b24315@students.iitmandi.ac.in>
2026-07-07 01:20:04 +0530

    Time: 15 ms (15.68%) | Memory: 44.8 MB (47.98%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -5,14 +5,13 @@ public:
         int j = 0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int cnt = 0;
+    
         while(i< g.size() && j< s.size()){
             if(s[j] >= g[i]){
-                cnt++;
                 i++;
             }
             j++;
         }
-        return cnt;
+        return i++;
     }
 };
