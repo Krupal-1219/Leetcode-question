f33b26fa52a95e2fb17b4d438207e706eb727616
Krupal Butala <b24315@students.iitmandi.ac.in>
2026-07-15 03:00:36 +0530

    Time: 11 ms (43.46%) | Memory: 18.1 MB (51.94%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+         int n = prices.size();
+       vector <vector<vector <int>>> dp(n+1, vector <vector <int>> (2,vector<int>(k+1,0)));
+
+       for(int i = n-1;i>=0;i--){
+            for(int buy = 0;buy<=1;buy++){
+                for(int trans =1 ;trans <=k;trans++){
+                    if(buy){
+                        dp[i][buy][trans] = max(-prices[i] + dp[i+1][0][trans], dp[i+1][1][trans]);
+                    }
+                    else dp[i][buy][trans] = max(prices[i] + dp[i+1][1][trans-1] , dp[i+1][0][trans]);
+                }
+            }
+       }
+       return dp[0][1][k]; 
+    }
+};
